31d942db547cf8d8c81bc24ae8e07382e81710f4
Xinzhu-Fang <xf15@hampshire.edu>
2017-08-01 14:58:06 -0400

    added fluorescence plotting

---
+++ sdk/transform_fluorescenece_trace_into_raster_format.m
@@ -0,0 +1,215 @@
+function current_raster_dir_name = transform_fluorescenece_trace_into_raster_format(fluorescenece_trace,session_id, stimuli,raster_directory_name)
+
+nwb_name = [num2str(session_id) '.nwb'];
+
+sampling_times = h5read(nwb_name,'/processing/brain_observatory_pipeline/DfOverF/imaging_plane_1/timestamps');
+
+stimuli_onset_times = h5read(nwb_name, strcat('/stimulus/presentation/', stimuli, '_stimulus/timestamps'));
+
+container_id = h5read(nwb_name,'/general/experiment_container_id');
+
+session_id = h5read(nwb_name,'/general/session_id');
+
+session_type = h5read(nwb_name,'/general/session_type');
+
+new_cell_specimen_ids = h5read(nwb_name, '/processing/brain_observatory_pipeline/ImageSegmentation/cell_specimen_ids');
+
+
+% create a head directory 'ratser/' that store all ratser formats
+
+if ~exist(raster_directory_name,'dir')
+    mkdir(raster_directory_name);
+end
+
+% under the head directory 'raster/', create a current direcotry that store
+% all raster formats returned from the current analysis
+current_raster_dir_name = [stimuli,'_', session_id ,'/'];
+current_raster_dir_name_full  = [raster_directory_name,current_raster_dir_name];
+
+if ~exist(current_raster_dir_name_full ,'dir')
+    mkdir(current_raster_dir_name_full );
+end
+
+% fetching some parameters (hardcoded inside the function) that define the raster data such as window
+% frames, sampling frequency, etc.
+
+parameters_for_cur_stimulus = fetch_stimuli_based_parameters(stimuli);
+
+% generate raster_labels, apply to every cell in the same session
+
+raster_labels = generate_raster_labels (nwb_name, stimuli);
+
+% create raster files
+
+for iCell = 1:size(fluorescenece_trace,2)
+    
+    cur_new_cell_id = new_cell_specimen_ids(iCell);
+    
+    cur_raster_file_name = [num2str(cur_new_cell_id), '.mat'];
+    % raster_data is a matrix of k dimensions of trials by n dimensions of
+    % time
+    raster_data = generate_raster_data(iCell, fluorescenece_trace, parameters_for_cur_stimulus, sampling_times, stimuli_onset_times);
+    
+    raster_site_info = generate_raster_site_info(container_id, parameters_for_cur_stimulus,session_id,session_type,cur_new_cell_id);
+    
+    save([current_raster_dir_name_full , cur_raster_file_name], 'raster_data', 'raster_labels', 'raster_site_info');
+    
+end
+
+fprintf ( [num2str(size(fluorescenece_trace,2)) ' cells transformed into raster formats'])
+fprintf ([' There are ' num2str(length(dir(current_raster_dir_name_full))-2) ' raster files in folder ' current_raster_dir_name_full])
+
+if num2str(size(fluorescenece_trace,2)) ~= num2str(length(dir(current_raster_dir_name_full))-2)
+    fprtintf ('You contaminated this folder sometime earlier. Anyway, you screwed up')
+end
+
+end
+
+
+%  generate raster_data
+
+function raster_data = generate_raster_data(i, fluorescenece_trace, parameters_for_cur_stimulus, sample_times, stimuli_onset_times)
+
+cur_cell_data = fluorescenece_trace (:,i);
+
+% preallocation of cell_matrix: a trial by time matrix of dfOVerF of a
+% single cell
+
+cur_cell_matrix = NaN * ones(size(stimuli_onset_times,1), parameters_for_cur_stimulus.sampling_points_after_onset...
+    - parameters_for_cur_stimulus.sampling_points_before_onset +1);
+
+onset = 1;
+% looking for the closet sampling time to stimuli onset time
+for iTrial = 1:size(stimuli_onset_times,1)  %6000
+    
+    curr_stimulus_onset = stimuli_onset_times(iTrial);
+    
+    while curr_stimulus_onset - sample_times(onset) > parameters_for_cur_stimulus.sample_interval
+        
+        onset = onset + 1;
+        
+    end
+    
+    cur_cell_matrix (iTrial,:) = cur_cell_data(onset + parameters_for_cur_stimulus.sampling_points_before_onset:...
+        onset + parameters_for_cur_stimulus.sampling_points_after_onset, 1)';
+    
+end
+raster_data = cur_cell_matrix;
+end
+
+% generate raster_site_info
+
+function raster_site_info = generate_raster_site_info(container_id, parameters_for_cur_stimulus,session_id,session_type,new_cell_id)
+
+raster_site_info = parameters_for_cur_stimulus;
+
+raster_site_info.container_id = container_id;
+
+raster_site_info.new_cell_id = new_cell_id;
+
+raster_site_info.session_id = session_id;
+
+raster_site_info.session_type = session_type;
+
+raster_site_info = orderfields(raster_site_info);
+
+end
+
+function parameters_for_cur_stimulus = fetch_stimuli_based_parameters(stimuli)
+
+% set parameters for raster_data
+
+stimuli_type = {'static_gratings';'drifting_gratings'};
+
+sample_interval = repelem(0.033, length(stimuli_type)).';  % 30 Hz two-photon movie
+
+stimuli_interval = [0.25; 3] ; % stimuli are shown every 250 ms
+
+starts = [-0.25; -0.25]; % window starts 250 ms before stimulus onset
+
+ends = [0.75; 2.75]; % window ends 75o ms after stimulus onset
+
+sampling_points_before_onset = NaN * ones(length(stimuli_type),1); % number of sampling points before the onset of stimuli
+
+for iStimulus_type = 1: length(stimuli_type)
+    % total of sampling time points taken before stimulus onset
+    sampling_points_before_onset(iStimulus_type) = round(starts(iStimulus_type)/sample_interval(iStimulus_type));
+    
+end
+
+position_of_sampling_point_at_onset = 1 - sampling_points_before_onset;
+
+sampling_points_after_onset = NaN * ones(length(stimuli_type),1);
+
+for iStimulus_type = 1: length(stimuli_type)
+    % total of sampling time points taken after stimulus onset
+    sampling_points_after_onset(iStimulus_type) = round(ends(iStimulus_type)/sample_interval(iStimulus_type));
+    
+end
+
+parameters_for_all_stimuli = table (sample_interval, stimuli_interval, starts, ends, sampling_points_before_onset ...
+    , sampling_points_after_onset,position_of_sampling_point_at_onset ,'RowNames', stimuli_type);
+
+parameters_for_cur_stimulus = table2struct(parameters_for_all_stimuli(stimuli,:));
+
+end
+
+function raster_labels = generate_raster_labels (nwb_name, stimuli)
+
+variables = h5read(nwb_name, strcat('/stimulus/presentation/', stimuli, '_stimulus/features'));
+
+% labels is a matrix of k dimensions of variables by n dimensions of trials
+levels = h5read(nwb_name,strcat('/stimulus/presentation/', stimuli, '_stimulus/data'));
+
+% In the case of drifting_gratings, there is this thrid variable called
+% blank sweep with two levels 1 and 0, which is redundant and discarded
+% here.
+
+if strcmp(stimuli,'drifting_gratings') == 1
+    
+    variables = variables(1:2,:);
+    
+    levels = levels(1:2,:);
+    
+end
+
+%     There were blank sweeps (i.e. mean luminance gray instead of grating)
+%     presented roughly once every 25 gratings,
+%     which have NaN for all three variables.
+%     We are converting the levels matrix into levels cellarray, where numbers are converted to string
+%     and NaNs are replaced with "blank"
+
+parsed_levels = cell (size(levels, 1),size(levels, 2));
+
+for iVariable = 1:size(levels, 1)
+    
+    for iLevel = 1:size(levels, 2)
+        
+        curr_level = levels(iVariable, iLevel);
+        
+        if isnan(curr_level)
+            
+            parsed_levels{iVariable}{iLevel} = 'blank';
+            
+        else
+            parsed_levels{iVariable}{iLevel} = num2str(curr_level);
+            
+        end
+    end
+end
+
+variables = cellstr(char(string(variables)));
+
+for iVariable = 1:size(variables,1)
+    
+    raster_labels.(char(strcat("stimulus_", variables(iVariable)))) = parsed_levels{iVariable};
+    
+end
+end
+
+
+
+
+
+
+
--- tutorial.m
+++ tutorial.m
@@ -1,11 +1,15 @@
 
 % A tutorial of:
-%  1) download needed files from brain observatory api and converting them to matlab tables
+%  1) downloading needed files from brain observatory api and converting them to matlab tables
 %  2) building a brain_observatory_cache object to a) get general information of the whole brain observatory dataset
-%                                                  b) select sessions by conditions such as brain areas, imaging depth and stimuli
+%                                                  b) select sessions by
+%                                                  conditions such as brain
+%                                                  areas, imaging depth and
+%                                                  stimuli
 %                                                  c) download nwb files of selected sessions
-%
-%% important information:
+%  3) importing imgaging data from nwb files to a) plot fluorescence traces
+%                                               b) transform them into raster formats for decoding
+%% important information about this dataset:
 %
 % an experiment container (named by allen institute) 
 % contains a set of subexperiments (defined by us as one subexperiment only 
@@ -27,7 +31,7 @@
 base_dir_name = '/om/user/xf15/Brain-Observatory-Toolbox/';
 
 % add path to sdk
-addpath([base_dir_name, 'sdk/data_access/'])
+addpath([base_dir_name, 'sdk/'])
 
 %% 1) download needed files from brain observatory api and converting them to matlab tables
 
@@ -45,14 +49,14 @@ boc = brain_observatory_cache (references)
 
 
 boc.get_total_of_containers()
-boc.get_summary_of_container_along_targeted_structures()
-boc.get_summary_of_containers_along_imaing_depths()
 boc.get_all_imaing_depths()
 boc.get_all_cre_lines()
 boc.get_all_targeted_structures()
 boc.get_all_session_types()
 boc.get_all_stimuli()
 boc.get_summary_of_containers_along_depths_and_structures()
+boc.get_summary_of_container_along_targeted_structures()
+boc.get_summary_of_containers_along_imaing_depths()
 
 %% 2b) select sessions by conditions such as brain areas, imaging depth and stimuli
 
@@ -64,50 +68,11 @@ boc.stimuli = 'drifting_gratings'
 boc.targeted_structure = 'VISp'
 boc.imaging_depth = 275
 
-% %  all filters are optional; all sessions will be retu% % reading in all manifest files and converting them to matlab tables
-% filePattern = fullfile('manifest_json','*.json');
-% theFiles = dir(filePattern);
-% for cur_file = 1 : length(theFiles)
-%     baseFileName = theFiles(cur_file).name;
-%     fullFileName = ['manifest_json/' baseFileName]
-%     fid = fopen(fullFileName);
-%     % read in json files as text
-%     raw =  fread(fid, inf);
-%     manifest_raw_text = char (raw');
-%     fclose(fid);
-%     % parse the json files to structs
-%     manifest_struct = jsondecode(manifest_raw_text);
-%     % convert the structs to Matlab tables
-%     all_manifests.(baseFileName(1:length(baseFileName)-5)) =  struct2table(manifest_struct);
-% end
-% 
-% % create cre_lines table and append it to exp_cont table (all experiments within an experiment container
-% % were performed on a single rat and shared imaging plane 
-% cont_table = all_manifests.experiment_containers;
-% cre_lines = cell(size(cont_table,1),1);
-% for i = 1:size(cont_table,1)
-%     donor_info = cont_table(i,:).specimen.donor;
-%     transgenic_lines_info = struct2table(donor_info.transgenic_lines);
-%     cre_lines(i,1) = transgenic_lines_info.name(string(transgenic_lines_info.transgenic_line_type_name) == 'driver' & ...
-%             contains(transgenic_lines_info.name, 'Cre'));
-% end
-% all_manifests.experiment_containers = [cont_table, cre_lines]
-% all_manifests.experiment_containers.Properties.VariableNames{'Var13'} = 'cre_lines'
-% 
-% % add cell_specimen_mapping table to all_manifests
-% filename = 'cell_specimen_mapping.csv';
-% cell_id_mapping_array = csvread(filename,2);
-% all_manifests.cell_specimen_mapping = array2table(cell_id_mapping_array,'VariableNames',...
-%     {'old_cell_id',	'session_A_new_cell_id', 'session_B_new_cell_id','session_C_new_cell_id'});
-% 
-% % save the manifest strucre
-% save('all_manifests','all_manifests');rned if no filter
-% %  is applied
-% % 
+% %  all filters are optional; all sessions will be returned if no filter
+% %  is applied 
 % % 
 % % you can also use brain_observatory_cache to look up manifest of
 % % selected session(s) by container_id or session_id
-% % 
 % %
 % boc = brain_observatory_cache(references)
 % boc.container_id = 527550471
@@ -129,3 +94,25 @@ boc.session_id = session_id
 boc.get_session()
 save_directory_name = [base_dir_name,'nwb_files/'];
 boc.get_session_data(save_directory_name);
+
+%% 3 import imgaging data from nwb files
+
+% add path to nwb files
+addpath ([base_dir_name, 'nwb_files/'])
+
+%% 3a) plot fluorescence traces of selcted cell in selected subexperiment
+
+session_id = 527745328;
+cell_specimen_id = 529022196;
+
+[raw,demixed,neuropil_corrected,DfOverF] = get_fluorescence_traces (session_id,cell_specimen_id);
+
+%% 3b) transform data of selected fluorescence trace of selected subexperiment into raster format
+
+raster_directory_name = [base_dir_name, 'raster/'];
+
+stimuli = 'drifting_gratings';
+fluorescence_trace = DfOverF;
+
+current_raster_dir_name = transform_fluorescenece_trace_into_raster_format(fluorescence_trace,...
+    session_id, stimuli,raster_directory_name);
