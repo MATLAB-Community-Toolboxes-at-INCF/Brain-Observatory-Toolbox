4c95086b27f4197a00f1e40882efcd667867fe6b
Dylan Richard Muir <dylan.richard.muir@gmail.com>
2018-01-30 10:41:51 +0100

    Added a utility function `bot.util.StimulusAlignedResp`, to compute stimulus-aligned response averages

---
+++ +bot/+util/StimulusAlignedResp.m
@@ -0,0 +1,76 @@
+function mfAlignedResp = StimulusAlignedResp(tStimulus, mfResp, fhMetric, vnFluoresenceTimeIndices)
+
+% StimulusAlignedResp - FUNCTION Return a matrix of responses, aligned over a set of stimulus presentations
+%
+% Usage: mfAlignedResp = StimulusAlignedResp(tStimulus, mfResp <, fhMetric, vnFluoresenceTimeIndices>)
+%
+% This function accepts a matrix `mfResp` of calcium responses (or inferred
+% spikes), and computes the average response for a set of stimulus
+% presentations defined in `tStimulus`.
+%
+% `mfResp` is a matrix [TxN], each column of which contains the response
+% over time for a single ROI (N ROIs and T time bins in total). `tStimulus`
+% is a table as returned by the `bot.session.get_stimulus_table()` method,
+% with stimulus presentation periods defined by columns 'start_frame' and
+% 'end_frame'. These indices refer to fluorescence frame indices, as
+% returned by the `bot.session.get_fluorescence_timestamps()` method.
+%
+% The response frames corresponding to a single stimulus presentation are
+% collated, and processed with the function `fhMetric`. By default
+% `fhMetric` is `nanmean`. An alternative metric can be provided as a
+% function handle with the signature `vfStimResp = fhMetric(mfRawResp)`.
+% The metric function must accept a [UxN] matrix `mfRawResp`, where N is
+% the number of ROIs as above, and U is an arbitrary number of frames. It
+% must return a [Ux1] vector `vfStimResp`, containing the stimulus response
+% for each ROI.
+%
+% The optional argument `vnFluoresenceTimeIndices` can be used to specify
+% which fluorescence time indices correspond to each row in `mfResp`. By
+% default, this is simply 1:size(mfResp, 1). This argument can be used if
+% `mfResp` contains only a temporal subset of the full session responses.
+%
+% `mfAlignedResp` will be a [VxN] matrix, where N is the number of ROIs and
+% V is the number of stimulus presentations.
+
+% - Set defaults, check arguments
+if ~exist('fhMetric', 'var') || isempty(fhMetric)
+   fhMetric = @(x)nanmean(x, 1);
+end
+
+if ~exist('vnFluoresenceTimeIndices', 'var') || isempty(vnFluoresenceTimeIndices)
+   vnFluoresenceTimeIndices = 1:size(mfResp, 1);
+end
+
+if numel(vnFluoresenceTimeIndices) ~= size(mfResp, 1)
+   error('BOT:Usage', ...
+         'The number of frames in `vnFluoresenceTimeIndices` must match the number of rows in `mfResp`.');
+end
+
+% - Which stimulus periods are dirty? i.e. overlapping fluorescence frames
+vbDirtyEnd = tStimulus{1:end-1, 'end_frame'} == tStimulus{2:end, 'start_frame'};
+vbDirtyStart = [false; vbDirtyEnd(1:end)];
+vbDirtyEnd = [vbDirtyEnd; false];
+
+% - Helper function for finding "clean" response frames (where the stimulus
+% did not change mid-frame)
+   function vnFluorFrames = clean_frames(nStart, nEnd, bDirtyStart, bDirtyEnd, ~)
+      [~, vnFluorFrames] = ismember((double(nStart) + double(bDirtyStart)):(double(nEnd) - double(bDirtyEnd)), vnFluoresenceTimeIndices);
+   end
+
+% - Get lists of clean fluorescence frames for each stimulus presentation period
+cvnFluorFrameInds = arrayfun(@clean_frames, tStimulus.('start_frame'), tStimulus.('end_frame'), vbDirtyStart, vbDirtyEnd, tStimulus.('frame'), 'UniformOutput', false);
+
+% - Helper function for computing the stimulus response metric over a set
+% of frames
+   function vfThisResp = stim_metric(vnFrameInds)
+      [~, vnRespInds] = ismember(vnFrameInds, vnFluoresenceTimeIndices);
+      vfThisResp = fhMetric(mfResp(vnRespInds, :));
+   end
+
+% - Process lists of fluorescence frames and compute metric
+cmfAlignedResp = cellfun(@stim_metric, cvnFluorFrameInds, 'UniformOutput', false);
+mfAlignedResp = cell2mat(cmfAlignedResp);
+
+end
+
+% --- END of StimulusAlignedResp.m ---
