70f127452d22e911a1925576df72e283e14a5254
vijayi <vijayi@ah-vijayi.dhcp.mathworks.com>
2021-03-20 22:45:45 -0400

    REFACTOR ITEM CLASS HIERARCHY (WIP) * Add missing files

---
+++ +bot/+items/+internal/Item.m
@@ -0,0 +1,116 @@
+classdef Item < handle & matlab.mixin.CustomDisplay
+    
+   %% PUBLIC INTERFACE
+   properties (SetAccess = protected)
+      metadata;         % Struct containing metadata about this item
+      id;               % ID of this item
+   end   
+   
+   %% SUBCLASS INTERFACE
+   
+   % Properties for matlab.mixin.CustomDisplay superclass implementation 
+   properties (Abstract, SetAccess = immutable, GetAccess = protected)
+       CORE_PROPERTIES_EXTENDED (1,:) string 
+       LINKED_ITEM_PROPERTIES (1,:) string        
+   end
+   
+   %% SUPERCLASS IMPLEMENTATION (matlab.mixin.CustomDisplay)
+   
+   properties (Constant, GetAccess = protected)
+       CORE_PROPERTIES = ["metadata" "id"];                     
+   end    
+   
+   methods (Access = protected)
+          function groups = getPropertyGroups(obj)
+            if ~isscalar(obj)
+                groups = getPropertyGroups@matlab.mixin.CustomDisplay(obj);
+            else                
+                
+                % Core properties
+                groups(1) = matlab.mixin.util.PropertyGroup([obj.CORE_PROPERTIES obj.CORE_PROPERTIES_EXTENDED]);
+                groups(2) = matlab.mixin.util.PropertyGroup(obj.LINKED_ITEM_PROPERTIES, 'Linked Dataset Items');
+                
+%                 if obj.is_nwb_cached()
+%                     description = '[cached]';
+%                 else
+%                     description = '[not cached]';
+%                 end
+%                 
+%                 propList = struct();
+%                 for prop = obj.lazy_property_list
+%                     propList.(prop) = description;
+%                 end
+%                 
+%                 groups(3) = matlab.mixin.util.PropertyGroup(propList, 'NWB data');
+            end
+        end
+       
+   end
+   
+   
+   %% DEVELOPER INTERFACE
+   properties (Hidden = true)
+      property_cache;
+   end
+   
+   methods (Access = protected)
+      function item = check_and_assign_metadata(item, id, manifest_table, type, varargin)
+         % - Check usage
+         if istable(id)
+            assert(size(id, 1) == 1, 'BOT:Usage', 'Only a single table row may be provided.')
+            table_row = id;
+         else
+            assert(isnumeric(id), 'BOT:Usage', '`nID` must be an integer ID.');
+            id = uint32(round(id));
+            
+            % - Locate an ID in the manifest table
+            matching_row = manifest_table.id == id;
+            if ~any(matching_row)
+               error('BOT:Usage', 'Item not found in %s manifest.', type);
+            end
+            
+            table_row = manifest_table(matching_row, :);
+         end
+         
+         % - Assign the table data to the metadata structure
+         item.metadata = table2struct(table_row);
+         item.id = item.metadata.id;
+      end
+   end   
+   
+   methods (Access = protected)
+      function data = fetch_cached(self, property, fun_access)
+         % fetch_cached - METHOD Access a cached property
+         %
+         % Usage: data = fetch_cached(self, property, fun_access)
+         %
+         % `property` is a string containing a property name. `fun_access`
+         % is a function handle that returns the property data, if not
+         % found in the cache. The property data will be returned from the
+         % local property cache, or else inserted after calling
+         % `fun_access`.
+         %
+         % `data` will be the cached property data.
+         
+         % - Check for cached property
+         if ~isfield(self.property_cache, property)
+            % - Use the access function
+            self.property_cache.(property) = fun_access();
+         end
+         
+         % - Return the cached property
+         data = self.property_cache.(property);
+      end
+      
+      function is_in_cache = in_cache(self, property)
+         % in_cache — METHOD Test if a property value has been cached
+         %
+         % Usage: is_in_cache = in_cache(self, property)
+         %
+         % `property` is a string containing a property name. `is_in_cache`
+         % will be `true` iff the property is present in the property
+         % cache.
+         is_in_cache = isfield(self.property_cache, property) && ~isempty(self.property_cache.(property));
+      end
+   end
+end
---
+++ +bot/+items/+internal/NWBItem.m
@@ -0,0 +1,48 @@
+classdef NWBItem < bot.items.internal.Item
+    
+    %% SUBCLASS INTERFACE
+    
+    properties (Abstract, SetAccess = immutable, GetAccess = protected)
+        NWB_FILE_PROPERTIES (1,:) string 
+    end
+
+    properties (Abstract, Dependent)
+        nwbIsCached (1,1) logical % true if NWB file corresponding to this item is already cached
+        nwbURL (1,1) string 
+    end       
+   
+%    properties (Hidden, Access=protected)
+%        well_known_file;           % Metadata about probe NWB files
+%       nwb_url;                   % URL for probe NWB file
+%       local_nwb_file_location;
+%    end                       
+    
+   %% SUPERCLASS OVERRIDES (matlab.mixin.CustomDisplay)
+    
+    methods (Access = protected)
+        function groups = getPropertyGroups(obj)
+            if ~isscalar(obj)
+                groups = getPropertyGroups@matlab.mixin.CustomDisplay(obj);
+            else                                
+                groups = getPropertyGroups@bot.items.internal.Item(obj);
+                
+                % NWB-bound properties
+                if obj.nwbIsCached
+                    description = '[cached]';
+                else
+                    description = '[not cached]';
+                end
+                
+                propList = struct();
+                for prop = obj.NWB_FILE_PROPERTIES
+                    propList.(prop) = description;
+                end
+                
+                groups(end+1) = matlab.mixin.util.PropertyGroup(propList, 'NWB data');                
+            end
+        end        
+    end
+
+    
+end
+
