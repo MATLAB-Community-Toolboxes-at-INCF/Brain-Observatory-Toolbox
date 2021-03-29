f91b43ffda629dd1dc4210ff5a604702117f07dc
vijayi <vijayi@ah-vijayi.dhcp.mathworks.com>
2021-03-28 21:08:02 -0400

    UPDATE ITEM TABLE DISPLAY * Further column reordering for ophys * Remove redundant variable for storage_directory (in ophysession)

--- +bot/+internal/manifest2item.m
+++ +bot/+internal/manifest2item.m
@@ -1,50 +1,56 @@
-function manifest = manifest2item(manifest)
+function tbl = manifest2item(tbl)
 % MANIFEST2ITEM Refine manifest table for display as an Item table
 
-assert(isa(manifest,'table'),"The input manifest must be a table object");
+assert(isa(tbl,'table'),"The input manifest must be a table object");
  
-varNames = string(manifest.Properties.VariableNames);
+varNames = string(tbl.Properties.VariableNames);
 
 
-manifest.Properties.UserData = struct();
+tbl.Properties.UserData = struct();
 
 %% Remove columns whose values are "redundant" (the same for all rows); store these as table properties instead
 redundantVarNames = string([]);
 
 for col = 1:length(varNames)    
     
-   if ~isstruct(manifest{1,col}) && ~iscell(manifest{1,col}) && numel(unique(manifest{:,col},'stable')) == 1 % all (non-compound) values in a column are equal 
+   if ~isstruct(tbl{1,col}) && ~iscell(tbl{1,col}) && numel(unique(tbl{:,col},'stable')) == 1 % all (non-compound) values in a column are equal 
        
        redundantVar = varNames(col);
        redundantVarNames = [redundantVarNames redundantVar]; %#ok<AGROW>
               
        %addfield(manifest.Properties.UserData,redundantVar);
-       manifest.Properties.UserData.(redundantVar) = manifest{1,col};
+       tbl.Properties.UserData.(redundantVar) = tbl{1,col};
    end     
 end    
 
-manifest = removevars(manifest, redundantVarNames);
+tbl = removevars(tbl, redundantVarNames);
+
+%% Remove columns repeating directory values (e.g. with OphysSession)
+
+containsDirVars = varNames(varNames.contains("directory"));
+tbl = removevars(tbl, containsDirVars);
+
 
 %% Initialize refined variable lists (names, values)
-refinedVars = setdiff(varNames,redundantVarNames,"stable");
-firstRowVals = table2cell(manifest(1,:));
+refinedVars = setdiff(varNames,[redundantVarNames containsDirVars],"stable");
+firstRowVals = table2cell(tbl(1,:));
 
 %% Convert ephys_structure_acronym from cell types to the most string type possible (string type for scalars, cell array of strings for string lists)
  
 varIdx = find(refinedVars.contains("structure_acronym"));
 if varIdx    
-    var = manifest.(refinedVars(varIdx));
+    var = tbl.(refinedVars(varIdx));
     
     if iscell(var)
         if iscell(var{1}) % Handle string lists with empty last value (e.g. ephys session)
             
             % convert to cell string array
             assert(all(cellfun(@(x)isempty(x{end}),var))); % all the cell string arrays end with an empty double array, for reason TBD
-            manifest.(refinedVars(varIdx)) = cellfun(@(x)x(1:end-1)',var,'UniformOutput',false);  % convert each cell element to string, skipping the ending empty double array
+            tbl.(refinedVars(varIdx)) = cellfun(@(x)x(1:end-1)',var,'UniformOutput',false);  % convert each cell element to string, skipping the ending empty double array
             
             % dereference to cell array of strings
-            for ii=1:height(manifest)
-                manifest{ii,varIdx}{1} = string(manifest{ii,varIdx}{1});
+            for ii=1:height(tbl)
+                tbl{ii,varIdx}{1} = string(tbl{ii,varIdx}{1});
             end
             
         else % cell string arrays (almost)
@@ -53,7 +59,7 @@ if varIdx
             
             var2 = var;
             [var2{cellfun(@(x)~ischar(x), var)}] = deal('');
-            manifest.(refinedVars(varIdx)) = string(var2);
+            tbl.(refinedVars(varIdx)) = string(var2);
         end
     end
 end    
@@ -67,18 +73,20 @@ containsIDVars = refinedVars(refinedVars.contains("id") & ~refinedVars.matches("
 countVars = refinedVars(refinedVars.contains("count"));
 dateVars = refinedVars(refinedVars.contains("date"));
 typeVars = refinedVars(refinedVars.contains("type"));
+stimVars = refinedVars(refinedVars.contains("stimulus"));
+nameVars = refinedVars(refinedVars.matches("name"));
 
 % Identify variables with compound data
 compoundVarIdxs = cellfun(@(x)isstruct(x) || iscell(x),firstRowVals);
 compoundVars = refinedVars(compoundVarIdxs);
 
 % Create new column order
-reorderedVars = ["id" containsIDVars countVars dateVars typeVars compoundVars]; 
-newVarOrder = ["id" containsIDVars typeVars setdiff(refinedVars,reorderedVars,"stable") dateVars countVars compoundVars];
+reorderedVars = ["id" containsIDVars countVars dateVars typeVars compoundVars nameVars stimVars]; 
+newVarOrder = ["id" containsIDVars typeVars stimVars setdiff(refinedVars,reorderedVars,"stable") dateVars countVars nameVars compoundVars];
 
 
 % Do reorder in one step
-manifest = manifest(:,newVarOrder);
+tbl = tbl(:,newVarOrder);
 
     
 end
