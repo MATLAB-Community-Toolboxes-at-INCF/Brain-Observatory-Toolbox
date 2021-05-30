83eb81db04a053fd70020562f938456005e8ce83
Vijay Iyer <viyer05@gmail.com>
2021-05-30 07:37:57 -0400

    add doc strings

--- +bot/+item/+internal/OnDemandState.m
+++ +bot/+item/+internal/OnDemandState.m
@@ -1,6 +1,10 @@
 classdef OnDemandState 
+%   A class which encodes internal state for on-demand property values
+       
     enumeration
-        Unavailable
+        %OnDemand       % <currently do not encode the initialized "on-demand" state of these properties since this is encoded by key absence within property cache>
+        
+        Unavailable     % an error occurred when trying to compute/access the data previously
     end
 end
 
