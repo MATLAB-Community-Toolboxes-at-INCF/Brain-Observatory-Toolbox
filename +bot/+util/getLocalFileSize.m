1384b61c689bf4c0ef4f401b762825995b0f5ba0
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-10-08 21:36:45 +0200

    Update utility functions for getting filesize
    Added documentation.
    Fix for local file if file does not exist

--- +bot/+util/getLocalFileSize.m
+++ +bot/+util/getLocalFileSize.m
@@ -1,4 +1,15 @@
 function fileSizeBytes = getLocalFileSize(filepath)
+%getLocalFileSize Get file size of a file on the local file system
+%
+%   fileSizeBytes = getLocalFileSize(filepath) returns the filesize in
+%   bytes for a file at the specified filepath.
+
     L = dir(filepath);
-    fileSizeBytes = L.bytes;
+    if isempty(L)
+        fileSizeBytes = []; % TODO: Consider whether to throw error instead.
+    elseif numel(L) > 1
+        error('Multiple files/folders matched the given filepath')
+    else
+        fileSizeBytes = L.bytes;
+    end
 end
--- +bot/+util/getWebFileSize.m
+++ +bot/+util/getWebFileSize.m
@@ -1,7 +1,11 @@
 function fileSizeBytes = getWebFileSize(webFileUrl)
+%getWebFileSize Get file size of a file on the web
+%
+%   fileSizeBytes = getWebFileSize(filepath) returns the filesize in
+%   bytes for a file at the specified web url
 
-    import matlab.net.URI;
-    import matlab.net.http.RequestMessage;
+    import matlab.net.URI
+    import matlab.net.http.RequestMessage
     
     uri = URI(webFileUrl);
     req = RequestMessage('HEAD');
@@ -9,5 +13,4 @@ function fileSizeBytes = getWebFileSize(webFileUrl)
     contentLengthField = response.getFields("Content-Length");
     
     fileSizeBytes = str2double(contentLengthField.Value);
-
 end
