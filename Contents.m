54aa995dbf15e03c0bb7759e15297e363d673c3f
ehennestad <ehennestad@gmail.com>
2022-12-07 17:34:01 +0100

    Create Contents.m

---
+++ Contents.m
@@ -0,0 +1,16 @@
+% Brain-Observatory-Toolbox
+% Version 0.9.3 (R2022a) 15-dec-2022
+%
+% Copyright (c) 2017, Ethan Meyers
+% ----------------------------------
+%
+% A MATLAB toolbox for accessing and using the neural recording 
+% public datasets from the Allen Brain Observatory.
+% 
+% Get started with the <a href="matlab:open EphysQuickstart" style="font-weight:bold">EphysQuickstart</a> & <a href="matlab:open OphysQuickstart" style="font-weight:bold">OphysQuickstart</a> guides.
+% 
+% See the Brain Observatory Toolbox applied to neuroscience data 
+% analysis in the <a href="matlab:open EphysDemo" style="font-weight:bold">EphysDemo</a> & <a href="matlab:open OphysDemo" style="font-weight:bold">OphysDemo</a>.
+% 
+% Learn how to use the Brain Observatory Toolbox with the 
+% <a href="matlab:open EphysTutorial" style="font-weight:bold">EphysTutorial</a>, <a href="matlab:open OphysTutorial" style="font-weight:bold">OphysTutorial</a> & <a href="matlab:open BehaviorTutorial" style="font-weight:bold">BehaviorTutorial</a>
