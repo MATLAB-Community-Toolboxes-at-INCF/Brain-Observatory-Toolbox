66e8353b1bab94879a461c7eeb03e765fa4acf60
Vijay Iyer <viyer05@gmail.com>
2023-02-27 18:49:51 -0500

    Add gateway live script, for interactive startup modes.
    Move live script examples back out of the bot.* package tree.
    
    Maintain discoverability from the bot package via a bot.callMeFirst() script.

---
+++ +bot/callMeFirst.m
@@ -0,0 +1,5 @@
+if batchStartupOptionUsed
+    disp("Getting started live script not available in batch mode.")
+else
+    open .\GettingStarted.mlx
+end
--- Contents.m
+++ Contents.m
@@ -1,5 +1,5 @@
 % Brain Observatory Toolbox
-% Version 0.9.3 (R2022a) 15-dec-2022
+% Version 0.9.3 (R2022b) 21-feb-2023
 %
 % Copyright (c) 2017, Ethan Meyers
 % ----------------------------------
@@ -7,14 +7,16 @@
 % A MATLAB toolbox for accessing and using the public neural 
 % recording datasets from the Allen Brain Observatory.
 % 
-% Get started with the <a href="matlab:open bot.EphysQuickstart" style="font-weight:bold">EphysQuickstart</a> & <a href="matlab:open bot.OphysQuickstart" style="font-weight:bold">OphysQuickstart</a> guides.
+% Get started with the <a href="matlab:open EphysQuickstart" style="font-weight:bold">EphysQuickstart</a> & <a href="matlab:open bot.OphysQuickstart" style="font-weight:bold">OphysQuickstart</a> guides.
 % 
 % See the Brain Observatory Toolbox applied to neuroscience data 
-% analysis in the <a href="matlab:open bot.demo.EphysDemo" style="font-weight:bold">EphysDemo</a> & <a href="matlab:open bot.demo.OphysDemo" style="font-weight:bold">OphysDemo</a>.
+% analysis in the <a href="matlab:open demos\EphysDemo" style="font-weight:bold">EphysDemo</a> & <a href="matlab:open demos\OphysDemo" style="font-weight:bold">OphysDemo</a>.
 % 
 % Learn how to use the Brain Observatory Toolbox with the 
-% <a href="matlab:open bot.tutorial.EphysTutorial" style="font-weight:bold">EphysTutorial</a>, <a href="matlab:open bot.tutorial.OphysTutorial" style="font-weight:bold">OphysTutorial</a> & <a href="matlab:open bot.tutorial.BehaviorTutorial" style="font-weight:bold">BehaviorTutorial</a>
+% <a href="matlab:open tutorials\EphysTutorial" style="font-weight:bold">EphysTutorial</a>, <a href="matlab:open tutorials\OphysTutorial" style="font-weight:bold">OphysTutorial</a> & <a href="matlab:open tutorials\BehaviorTutorial" style="font-weight:bold">BehaviorTutorial</a>
 %
+%   Getting started
+%     bot.callMeFirst       - Show Getting Started live script (if using desktop environment)
 %
 %   Listing item information:
 %     bot.listExperiments   - List information about all experiments (Ophys)
