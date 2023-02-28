8c72ff226496173234be874a72bf2247bda838f1
Vijay <vijayi@mathworks.com>
2023-02-28 11:30:12 +0000

    Rename gateway live script to README to distinguish from examples/functions
    Move gateway live script into bot package, i.e. bot.README
    
    Move quickstart guides into dedicated folders, so no code in repo folder. 
    
    Result: everything is either namespaced or linked from namespaced README
    
    Update Contents.m accordingly

--- +bot/callMeFirst.m
+++
@@ -1,7 +0,0 @@
-% Open Getting Started live script
-
-if batchStartupOptionUsed
-    disp("Getting started live script not available in batch mode.")
-else
-    open(fullfile('.',filesep,'GettingStarted.mlx'));
-end
