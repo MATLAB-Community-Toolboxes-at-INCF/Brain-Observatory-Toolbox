7800bff6e0baa7b52fc0396f01f4b193a47a7515
Vijay Iyer <viyer05@gmail.com>
2023-03-09 22:39:43 -0500

    add some clarifying comments

--- +bot/README.m
+++ +bot/README.m
@@ -2,7 +2,7 @@
 % For BOT testers: run gateway live script in batch MATLAB mode
 
 if batchStartupOptionUsed
-    bot.internal.README(); 
+    bot.internal.README(); % run the code
 else
-    edit bot.internal.README;
+    edit bot.internal.README; % view the live script
 end
