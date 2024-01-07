d562217d8f9186e8b4e0eb617f58f399b50b5f63
ehennestad <ehennestad@gmail.com>
2024-01-07 20:54:14 +0100

    Updated OphysNWBFile and added BehaviorNWBFile

---
+++ +bot/+internal/+nwb/+file/+vb/BehaviorNWBFile.m
@@ -0,0 +1,192 @@
+classdef BehaviorNWBFile < bot.internal.nwb.LLNWBData
+% BehaviorNWBFile - Provides the data from a Visual Behavior session
+%
+%   This class provides data from the behavior only sessions.
+
+
+%   Todo: 
+%   [ ] Verify that it can be used for both neuropixels and ophys
+%       behavior-only sessions.
+
+    properties %(SetAccess = private, Transient)
+        
+        % Metadata:
+        Metadata bot.internal.OnDemandProperty = ...
+            bot.internal.OnDemandProperty([1,1], 'struct')
+
+        SubjectDetails bot.internal.OnDemandProperty = ...
+            bot.internal.OnDemandProperty([1,1], 'struct')
+
+        TaskParameters bot.internal.OnDemandProperty = ...
+            bot.internal.OnDemandProperty([1,1], 'struct')
+
+        EyeTrackingRigGeometry bot.internal.OnDemandProperty = ...
+            bot.internal.OnDemandProperty([1,1], 'struct')
+
+        % Behavioral data:
+        Licks = bot.internal.OnDemandProperty([nan,1], 'timetable')
+        Rewards = bot.internal.OnDemandProperty([nan,2], 'timetable')
+        EyeTracking = bot.internal.OnDemandProperty([nan,15], 'timetable')
+        RunningSpeed = bot.internal.OnDemandProperty([nan,1], 'timetable')
+        
+        % Interval data:
+        StimulusTimes = bot.internal.OnDemandProperty([nan,1], 'duration')
+        StimulusPresentation = bot.internal.OnDemandProperty([nan,1], 'timetable')
+        StimulusTemplates = bot.internal.OnDemandProperty([nan,2], 'table')
+        Trials = bot.internal.OnDemandProperty([nan,22], 'table')
+    end
+
+    properties (Hidden) % Properties that are not shown to users.
+        StimulusNames bot.internal.OnDemandProperty
+        StimulusTemplatesWarped bot.internal.OnDemandProperty
+    end
+
+    properties (Access = protected)
+        PropertyToDatasetMap = dictionary(...
+                              'Metadata', '/general/experiment_description', ...
+                        'SubjectDetails', '/general/subject/subject_id', ...
+                        'TaskParameters', '/general/task_parameters/stimulus', ...
+                'EyeTrackingRigGeometry', '/processing/eye_tracking_rig_metadata/eye_tracking_rig_metadata/camera_position', ...
+                                 'Licks', '/processing/licking/licks/data', ...
+                               'Rewards', '/processing/rewards/autorewarded/data', ...
+                           'EyeTracking', '/acquisition/EyeTracking/pupil_tracking/data', ...
+                          'RunningSpeed', '/processing/running/speed/data', ...
+                         'StimulusTimes', '/processing/stimulus/timestamps/data', ...
+                  'StimulusPresentation', '/stimulus/presentation/.*/data', ...
+                     'StimulusTemplates', '/stimulus/templates/.*/unwarped', ...
+               'StimulusTemplatesWarped', '/stimulus/templates/.*/data', ...
+                         'StimulusNames', '/stimulus/templates/.*/control_description', ...
+                                'Trials', '/intervals/trials/start_time' )
+
+        PropertyProcessingFcnMap = dictionary(...
+                              'Metadata', 'readNwbMetadata', ...
+                        'SubjectDetails', 'readSubjectDetails', ...
+                        'TaskParameters', 'readTaskParameters', ...
+                'EyeTrackingRigGeometry', 'readEyetrackingMetadata', ...
+                                 'Licks', 'postProcessLicks', ...
+                          'RunningSpeed', 'postProcessRunningSpeed', ...
+                               'Rewards', 'readRewardsTimeTable', ...
+                           'EyeTracking', 'readEyeTrackingTimeTable', ...
+                         'StimulusTimes', 'postProcessStimulusTimes', ...
+                  'StimulusPresentation', 'postProcessStimulusPresentation', ...
+                     'StimulusTemplates', 'postProcessStimulusTemplates', ...
+                                'Trials', 'readTrialsTable' )
+    end
+    
+    methods (Access = ?bot.internal.nwb.LLNWBData)
+            
+        function metadata = readNwbMetadata(obj, ~)
+            metadata = bot.internal.nwb.reader.readGeneralMetadata(obj.FilePath);
+        end
+
+        function taskParameters = readTaskParameters(obj, ~)
+            taskParameters = bot.internal.nwb.reader.readTaskMetadata(obj.FilePath);
+        end
+
+        function subjectDetails = readSubjectDetails(obj, ~)
+            subjectDetails = bot.internal.nwb.reader.readSubjectMetadata(obj.FilePath);
+        end
+        
+        function data = readEyetrackingMetadata(obj, ~)
+            data = bot.internal.nwb.reader.readRigMetadata(obj.FilePath);
+        end
+
+        function data = readRewardsTimeTable(obj, ~)
+            data = bot.internal.nwb.reader.vb.read_rewards_timetable(obj.FilePath);
+        end
+
+        function data = readEyeTrackingTimeTable(obj, ~) 
+            data = bot.internal.nwb.reader.vb.read_eyetracking_timetable(obj.FilePath);
+        end
+
+        function data = postProcessLicks(~, data)
+            data.Properties.VariableNames = {'frame'};
+        end
+           
+        function data = postProcessRunningSpeed(~, data)
+            data.Properties.VariableNames = {'speed'};
+        end
+
+        function data = postProcessStimulusPresentation(obj, data)
+            names = obj.fetchData("StimulusNames");
+            uniqueValues = unique(data.StimulusPresentation);
+            data.StimulusPresentation = categorical(data.StimulusPresentation,uniqueValues,names);
+        end
+
+        function data = readTrialsTable(obj, ~)
+            data = bot.internal.nwb.reader.vb.read_trials_timetable(obj.FilePath);
+        end
+
+        function data = postProcessStimulusTimes(~, data)
+            data = data.timestamps;
+        end
+
+        function data = postProcessStimulusTemplates(obj, data)
+        % postProcessStimulusTemplates - Add warped templates and create table
+            imageNames = obj.fetchData("StimulusNames");
+            warped = obj.fetchData("StimulusTemplatesWarped");
+            
+            data = uint8(data);
+            data = permute(data, [2,1,3]);
+            warped = permute(warped, [2,1,3]);
+
+            imageSize = size(data);
+
+            unwarpedImages = mat2cell(data, imageSize(1), imageSize(2), ones(imageSize(3),1));
+            warpedImages = mat2cell(warped, imageSize(1), imageSize(2), ones(imageSize(3),1));
+
+            data = table(squeeze(unwarpedImages), squeeze(warpedImages), ...
+                'VariableNames', {'unwarped', 'warped'}, 'RowNames', imageNames);
+        end
+    end
+
+    % % methods (Access = {?bot.behavior.internal.mixin.HasLinkedFile, ?bot.behavior.internal.LinkedFile})
+    % %     function propertyGroups = getPropertyGroups(obj)
+    % % 
+    % %         import matlab.mixin.util.PropertyGroup
+    % %         propertyGroups = matlab.mixin.util.PropertyGroup.empty;
+    % % 
+    % %         grouping = obj.getPropertyGroupingMap();
+    % %         groupNames = grouping.keys()';
+    % % 
+    % %         for groupName = groupNames
+    % %             propList = struct;
+    % % 
+    % %             propertyNames = grouping{groupName};
+    % % 
+    % %             for j = 1:numel(propertyNames)
+    % %                 thisPropName = propertyNames{j};
+    % %                 thisPropValue = obj.(thisPropName);
+    % % 
+    % %                 % Customize the property display if the value is empty.
+    % %                 if isempty(thisPropValue)
+    % %                     if obj.isInitialized()
+    % %                         thisPropValue = categorical({'<unknown>  (unavailable)'});
+    % %                     else
+    % %                         thisPropValue = categorical({'<unknown>  (download required)'});
+    % %                     end
+    % %                 end
+    % % 
+    % %                 propList.(thisPropName) = thisPropValue;
+    % %             end
+    % % 
+    % %             %displayName = obj.DisplayName;
+    % %             groupTitle = "Linked NWB Data (" + groupName + ")";
+    % %             %groupTitle = sprintf('<strong>%s:</strong>', groupTitle);
+    % %             propertyGroups(end+1) = PropertyGroup(propList, groupTitle); %#ok<AGROW>
+    % %         end
+    % %     end
+    % % end
+
+    methods (Access = protected)
+
+        function map = getPropertyGroupingMap(obj)
+            map = dictionary(...
+                'NWB Metadata', {{'Metadata', 'SubjectDetails', 'TaskParameters', 'EyeTrackingRigGeometry'}}, ...
+                'Behavior', {{'Licks', 'Rewards', 'EyeTracking', 'RunningSpeed'}}, ...
+                'Intervals', {{'StimulusTimes', 'StimulusPresentation', 'Trials'}}, ...
+                'Stimulus', {{'StimulusTemplates'}} );
+        end
+    end
+
+end
---
+++ +bot/+internal/+nwb/+file/+vb/OphysNWBFile.m
@@ -0,0 +1,95 @@
+classdef OphysNWBFile < bot.internal.nwb.file.vb.BehaviorNWBFile
+
+    properties
+
+        % Physiology data
+        TwoPhotonParameters = bot.internal.OnDemandProperty([1,1], 'struct')
+        CellSpecimenTable = bot.internal.OnDemandProperty([missing, 13], 'table')
+        FovAverageProjection = bot.internal.OnDemandProperty([missing, missing], 'uint8')
+        FovMaximumProjection = bot.internal.OnDemandProperty([missing, missing], 'uint8')
+        SegmentationMaskImage = bot.internal.OnDemandProperty([missing, missing], 'int64')
+        FluorescenceTracesDemixed = bot.internal.OnDemandProperty([missing, 1], 'timetable')
+        FluorescenceTracesCorrected = bot.internal.OnDemandProperty([missing, 1], 'timetable')
+        FluorescenceTracesDFF = bot.internal.OnDemandProperty([missing, 1], 'timetable')
+        FluorescenceTracesNeuropil = bot.internal.OnDemandProperty([missing, 1], 'timetable')
+        FluorescenceEvents = bot.internal.OnDemandProperty([missing, 1], 'timetable')     
+        MotionCorrection = bot.internal.OnDemandProperty([missing, 2], 'timetable')
+    
+        % Todo?
+        % CellSpecimenIds            % Vector of cell specimen IDs recorded in this session
+        % RoiMasks
+    end
+
+    properties (Hidden) % Properties that are not shown to users.
+        MotionCorrectionDx bot.internal.OnDemandProperty
+        MotionCorrectionDy bot.internal.OnDemandProperty
+    end
+
+    methods (Access = protected)
+        
+        function expandPropertyMaps(obj)
+        % expandPropertyMaps - Expand propertymaps with ophys properties
+        %
+        %   % Expand the property maps defined for the behavior NWB file
+        %   with ophys specific properties
+
+            rootGroup = "/processing/ophys";
+            
+            obj.PropertyToDatasetMap("TwoPhotonParameters")         = "/general/optophysiology/imaging_plane_1/description";
+            obj.PropertyToDatasetMap("CellSpecimenTable")           = rootGroup + "/image_segmentation/cell_specimen_table/id";
+            obj.PropertyToDatasetMap("FovAverageProjection")        = rootGroup + "/images/average_image";
+            obj.PropertyToDatasetMap("FovMaximumProjection")        = rootGroup + "/images/max_projection";
+            obj.PropertyToDatasetMap("SegmentationMaskImage")       = rootGroup + "/images/segmentation_mask_image";                  
+            obj.PropertyToDatasetMap("FluorescenceTracesNeuropil")  = rootGroup + "/neuropil_trace/traces/data";
+            obj.PropertyToDatasetMap("FluorescenceTracesCorrected") = rootGroup + "/corrected_fluorescence/traces/data";
+            obj.PropertyToDatasetMap("FluorescenceTracesDemixed")   = rootGroup + "/demixed_trace/traces/data";
+            obj.PropertyToDatasetMap("FluorescenceTracesDFF")       = rootGroup + "/dff/traces/data";
+            obj.PropertyToDatasetMap("FluorescenceEvents")          = rootGroup + "/event_detection/data";
+            obj.PropertyToDatasetMap("MotionCorrection")            = rootGroup + "/ophys_motion_correction_x/data";
+            obj.PropertyToDatasetMap("MotionCorrectionDx")          = rootGroup + "/ophys_motion_correction_x/data";
+            obj.PropertyToDatasetMap("MotionCorrectionDy")          = rootGroup + "/ophys_motion_correction_y/data";
+
+            obj.PropertyProcessingFcnMap("CellSpecimenTable") = "readCellSpecimenTable";
+            obj.PropertyProcessingFcnMap("MotionCorrection") = "postprocessMotionCorrection";
+            obj.PropertyProcessingFcnMap("TwoPhotonParameters") = "readOphysMetadata";
+        end
+
+        function initializeProperties(obj)
+            obj.expandPropertyMaps()
+            initializeProperties@bot.internal.nwb.file.vb.BehaviorNWBFile(obj)
+        end
+
+        function map = getPropertyGroupingMap(obj)
+            map = getPropertyGroupingMap@bot.internal.nwb.file.vb.BehaviorNWBFile(obj);
+            map{"NWB Metadata"}{end+1} = "TwoPhotonParameters"; 
+            map("Physiology") = {{...
+                'CellSpecimenTable', ...
+                'FovAverageProjection', ...
+                'FovMaximumProjection', ...
+                'SegmentationMaskImage', ...                
+                'FluorescenceTracesNeuropil', ...
+                'FluorescenceTracesCorrected', ...
+                'FluorescenceTracesDemixed', ...
+                'FluorescenceTracesDFF', ...
+                'FluorescenceEvents', ...
+                'MotionCorrection' ...
+                }};
+        end
+    end
+    
+    methods (Access = ?bot.internal.nwb.LLNWBData)
+        function metadata = readOphysMetadata(obj, ~)
+            metadata = bot.internal.nwb.reader.readOphysMetadata(obj.FilePath);
+        end
+
+        function data = readCellSpecimenTable(obj, ~)
+            data = bot.internal.nwb.reader.vb.read_cell_specimen_table(obj.FilePath);
+        end
+
+        function data = postprocessMotionCorrection(obj, data)
+            data.Properties.VariableNames = {'x'};
+            y = obj.fetchData("MotionCorrectionDy");
+            data.y = y.MotionCorrectionDy;
+        end
+    end
+end
