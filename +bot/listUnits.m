c3bf007aa3f7a1f0a8beb233ba07d5557f10e354
ehennestad <ehennestad@gmail.com>
2024-02-18 11:40:12 +0100

    Update listUnits.m
    Included option for getting Visual Behavior Units
    Updated docstring

--- +bot/listUnits.m
+++ +bot/listUnits.m
@@ -1,25 +1,56 @@
-% Retrieve table of Neuropixels probes information for an Allen Brain Observatory dataset
+% Retrieve table of ephys unit information for an Allen Brain Observatory dataset
 %
-% Supports the Visual Coding Neuropixels [1] dataset from the Allen Brain Observatory [2]. 
+% Supports the Visual Coding Neuropixels [1] dataset and the Visual 
+% Behavior Neuropixels [2] dataset from the Allen Brain Observatory [3].
 %
-% Web data accessed via the Allen Brain Atlas API [3]. 
+% Usage:
+%    units = bot.listUnits() returns a table of unit information for the
+%       Visual Coding dataset
 %
-% [1] Copyright 2019 Allen Institute for Brain Science. Visual Coding Neuropixels dataset. Available from: https://portal.brain-map.org/explore/circuits/visual-coding-neuropixels
-% [2] Copyright 2016 Allen Institute for Brain Science. Allen Brain Observatory. Available from: https://portal.brain-map.org/explore/circuits
-% [3] Copyright 2015 Allen Institute for Brain Science. Allen Brain Atlas API. Available from: https://brain-map.org/api/index.html
+%    units = bot.listUnits(datasetName) returns a table of unit information for 
+%       the specified dataset. datasetName can be "VisualCoding" or
+%       "VisualBehavior" (Default = "VisualCoding")
 %
-%% function units = listUnits(include_metrics)
-function unitsTable = listUnits(include_metrics)
-arguments
-    include_metrics logical = false;
-end
+%    units = bot.listUnits(datasetName, include_metrics) additionally specifies
+%       whether to include unit metrics or not (Default = false).
+%
+% Web data accessed via the Allen Brain Atlas API [4]. 
+%
+% [1] Copyright 2019 Allen Institute for Brain Science. Visual Coding Neuropixels dataset. 
+%       Available from: https://portal.brain-map.org/explore/circuits/visual-coding-neuropixels
+%
+% [2] Copyright 2023 Allen Institute for Brain Science. Visual Behavior Neuropixels dataset. 
+%       Available from: https://portal.brain-map.org/circuits-behavior/visual-behavior-neuropixels
+%
+% [3] Copyright 2016 Allen Institute for Brain Science. Allen Brain Observatory. 
+%       Available from: https://portal.brain-map.org/explore/circuits
+%
+% [4] Copyright 2015 Allen Institute for Brain Science. Allen Brain Atlas API. 
+%       Available from: https://brain-map.org/api/index.html
 
-manifest = bot.item.internal.Manifest.instance('ephys');
-   unitsTable = manifest.ephys_units;
-   
-   % - Trim metrics from table
-   if ~include_metrics
-       unitsTable = removevars(unitsTable, bot.item.Unit.METRIC_PROPERTIES);
-   end
-end
+function unitsTable = listUnits(datasetName, includeMetrics)
 
+    arguments 
+        datasetName (1,1) bot.item.internal.enum.Dataset = "VisualCoding"
+        includeMetrics logical = false;
+    end
+
+    import bot.item.internal.enum.Dataset
+
+    % Get the metadata manifest for the selected dataset
+    manifest = bot.item.internal.Manifest.instance('Ephys', datasetName);
+
+    % Get the units item table
+    if datasetName == "VisualCoding"
+        unitsTable = manifest.ephys_units;
+    else
+        unitsTable = manifest.Units;
+    end
+
+    % - Trim metrics from table
+    if ~includeMetrics
+        metricProperties = bot.item.Unit.METRIC_PROPERTIES;
+        presentMetricProperties = intersect(metricProperties, unitsTable.Properties.VariableNames);
+        unitsTable = removevars(unitsTable, presentMetricProperties);
+    end
+end
