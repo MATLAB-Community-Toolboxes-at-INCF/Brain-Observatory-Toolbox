12cc12581ce404b6ab838829dd6329ac34d1cb21
ehennestad <ehennestad@gmail.com>
2024-11-24 20:59:18 +0100

    ...

--- tools/tasks/testToolbox.m
+++ tools/tasks/testToolbox.m
@@ -6,9 +6,14 @@ function testToolbox(varargin)
     botPrefs = bot.util.getPreferences();
     if botPrefs.CacheDirectory == "" || ~isfolder(botPrefs.CacheDirectory)
         botPrefs.DialogMode = "Command Window";
-        tempCache = fullfile(tempdir, 'bot-cache');
-        if ~isfolder(tempCache); mkdir(tempCache); end
+        tempCache = fullfile(tempdir, 'bot_cache');
+        if ~isfolder(tempCache)
+            fprintf('Making cache directory "%s"', tempCache)
+            mkdir(tempCache); 
+        end
         cleanupObj = onCleanup(@(fp) rmdir(tempCache, "s"));
+        botPrefs.CacheDirectory = tempCache;
+        disp(botPrefs)
     end
 
     matbox.tasks.testToolbox(projectRootDirectory, varargin{:})
